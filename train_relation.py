2a1340d608fe00dae12cf5cbedd1898324d5c196
s12you <s1212mn@gmail.com>
2024-05-27 16:04:45 +0800

    modified:   .gitignore 	modified:   PS_command/gpu_task.sh
    	modified:   train_relation.py

--- train_relation.py
+++ train_relation.py
@@ -310,7 +310,7 @@ if __name__=="__main__":
     data_dir = os.path.normpath(arg.data_dir)
     database = os.path.basename(data_dir)
     # dataset=[10,50,90,150,500]
-    dataset=[10]
+    dataset=[50,90]
     print(arg.data_dir)
     print("Train on:",arg.encoder_model_name)
     print("Train on:",arg.transform_model_name)
