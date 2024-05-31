e7c147a65b8056af9f6cca5ee19860497706db36
s12you <s1212mn@gmail.com>
2024-05-31 22:00:13 +0800

    modified:   PS_command/gpu_task.sh 	modified:   train_relation.py

--- train_relation.py
+++ train_relation.py
@@ -165,7 +165,7 @@ if __name__=="__main__":
     arg = ar.parse_args()
     data_dir = os.path.normpath(arg.data_dir)
     database = os.path.basename(data_dir)
-    dataset=[5,90,150]
+    dataset=[150]
     print(arg.data_dir)
     print("Train on:",arg.model_name)
     train_on_dataset(arg,dataset)
