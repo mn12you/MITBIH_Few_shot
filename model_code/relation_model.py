03fccf80f5f2074c38319bb5ca041d010a501c94
mn12you <s1212mn@gmail.com>
2024-05-31 20:52:17 +0800

    modified:   args.py 	modified:   model_code/relation_model.py 	modified:   train_relation.py

--- model_code/relation_model.py
+++ model_code/relation_model.py
@@ -1,35 +1,13 @@
 import torch
 import torch.nn as nn
 import os
-
-class RelationNet(nn.Sequential):
-     def __init__(self,kernel_size=5,num_classes=5,input_channels=128,inplanes=128, stride=2):
-        layer_temp=[]
-        input_channels=inplanes*2
-        for i in range(2):
-            if i>0:
-                input_channels=input_channels//2
-            layer_temp.append(nn.Conv1d(input_channels, inplanes,kernel_size=kernel_size,
-                                        stride=stride, padding=(kernel_size-1)//2,bias=False))
-            layer_temp.append(nn.BatchNorm1d(inplanes))
-            layer_temp.append(nn.ReLU(inplace=True))
-            layer_temp.append(nn.MaxPool1d(kernel_size=3, stride=2, padding=1))
-
-        layer_temp.append(nn.Flatten())
-        layer_temp.append(nn.Linear(input_channels*5,128))
-        layer_temp.append(nn.ReLU(inplace=True))
-        layer_temp.append(nn.Linear(128,1))
-        layer_temp.append(nn.Sigmoid())
-        super().__init__(*layer_temp)
-    
-
 class SembedNet(nn.Sequential):
    
 
     def __init__(self,input_channels=1):
         layer_temp=[]
         layer_temp.append(nn.Conv1d(input_channels, out_channels=6,kernel_size=3,
-                                        stride=1, padding=1))
+                                      stride=1, padding=1))
         layer_temp.append(nn.ReLU())
         layer_temp.append(nn.MaxPool1d(kernel_size=2, stride=2, padding=0))
 
@@ -47,33 +25,47 @@ class SembedNet(nn.Sequential):
         layer_temp.append(nn.Linear(in_features=384,out_features=16))
         layer_temp.append(nn.Linear(in_features=16,out_features=5))
         layer_temp.append(nn.Softmax(dim=1))
-        # for i in range(2):
-        #     if i >0:
-        #         input_channels=inplanes
-        #     layer_temp.append(nn.Conv1d(input_channels, inplanes,kernel_size=kernel_size,
-        #                                 stride=stride, padding=(kernel_size-1)//2,bias=False))
-        #     layer_temp.append(nn.BatchNorm1d(inplanes))
-        #     layer_temp.append(nn.ReLU(inplace=True))
-        #     layer_temp.append(nn.MaxPool1d(kernel_size=3, stride=2, padding=1))
-        # for i in range(2):
-        #     layer_temp.append(nn.Conv1d(input_channels, inplanes,kernel_size=kernel_size,
-        #                                 stride=stride, padding=(kernel_size-1)//2,bias=False))
-        #     layer_temp.append(nn.BatchNorm1d(inplanes))
-        #     layer_temp.append(nn.ReLU(inplace=True))
         super().__init__(*layer_temp)
 
-class transform_fully(nn.Sequential):
-   
 
-    def __init__(self,input_channels=1):
+class LMUEBCNet(nn.Sequential):
+    
+    def __init__(self):
         layer_temp=[]
-        layer_temp.append(nn.Linear(in_features=1,out_features=1))
+        layer_temp.append(nn.Conv1d(1, out_channels=6,kernel_size=3,
+                                        stride=2, padding=1))
+        layer_temp.append(nn.ReLU())
+        layer_temp.append(nn.MaxPool1d(kernel_size=2, stride=2, padding=0))
+
+        layer_temp.append(nn.Conv1d(6, out_channels=8,kernel_size=3,
+                                        stride=1, padding=1))
+        layer_temp.append(nn.ReLU())
+        layer_temp.append(nn.MaxPool1d(kernel_size=2, stride=2, padding=0))
+
+        layer_temp.append(nn.Conv1d(8, out_channels=12,kernel_size=3,
+                                        stride=1, padding=1))
+        layer_temp.append(nn.Conv1d(12, out_channels=12,kernel_size=3,
+                                        stride=1, padding=1))
+        layer_temp.append(nn.ReLU())
+        layer_temp.append(nn.MaxPool1d(kernel_size=2, stride=2, padding=0))
+
+        layer_temp.append(nn.Conv1d(12, out_channels=12,kernel_size=3,
+                                        stride=1, padding=1))
+        layer_temp.append(nn.Conv1d(12, out_channels=12,kernel_size=3,
+                                        stride=1, padding=1))
+        layer_temp.append(nn.ReLU())
+        layer_temp.append(nn.MaxPool1d(kernel_size=2, stride=2, padding=0))
+
+        layer_temp.append(nn.Flatten())
+        layer_temp.append(nn.Linear(in_features=96,out_features=16))
         super().__init__(*layer_temp)
 
-class SiameseNetwork(nn.Module):
+
+
+class Siamese_Sembed(nn.Module):
 
     def __init__(self):
-        super(SiameseNetwork, self).__init__()
+        super(Siamese_Sembed, self).__init__()
         layer_temp=[]
         layer_temp.append(nn.Conv1d(1, out_channels=6,kernel_size=3,
                                         stride=1, padding=1))
@@ -92,75 +84,57 @@ class SiameseNetwork(nn.Module):
 
         layer_temp.append(nn.Flatten())
         layer_temp.append(nn.Linear(in_features=384,out_features=16))
-
         self.encoder = torch.nn.Sequential(*layer_temp)
-
         # add linear layers to compare between the features of the two images
         self.fc =nn.Linear(1, 1)
-
         self.sigmoid = nn.Sigmoid()
-
-        
-    # def init_weights(self, m):
-    #     if isinstance(m, nn.Linear):
-    #         torch.nn.init.xavier_uniform_(m.weight)
-    #         m.bias.data.fill_(0.01)
 
     def forward_once(self, x):
         output = self.encoder(x)
         output = output.view(output.size()[0], -1)
-
         return output
 
     def forward(self, input1, input2):
-
         # get two images' features
         output1 = self.forward_once(input1)
         output2 = self.forward_once(input2)
-
-        # concatenate both images' features
-        # output = torch.cat((output1, output2), 1)
         output=torch.norm((output1-output2), p=1, dim=1).unsqueeze(-1)
-
         # pass the concatenation to the linear layers
         output = self.fc(output)
-
         # pass the out of the linear layers to sigmoid layer
         output = self.sigmoid(output)
-
-        
         return output
-    
-
-# def RNet_1d(device,**kwargs):
-#     model =RNet( **kwargs).to(device)
-
-#     return model
-
-
-
-class SiameseNetwork_LMU(nn.Module):
+class Siamese_LMU(nn.Module):
 
     def __init__(self):
-        super(SiameseNetwork, self).__init__()
+        super(Siamese_LMU, self).__init__()
         layer_temp=[]
         layer_temp.append(nn.Conv1d(1, out_channels=6,kernel_size=3,
+                                        stride=2, padding=1))
+        layer_temp.append(nn.ReLU())
+        layer_temp.append(nn.MaxPool1d(kernel_size=2, stride=2, padding=0))
+
+        layer_temp.append(nn.Conv1d(6, out_channels=8,kernel_size=3,
                                         stride=1, padding=1))
         layer_temp.append(nn.ReLU())
         layer_temp.append(nn.MaxPool1d(kernel_size=2, stride=2, padding=0))
 
-        layer_temp.append(nn.Conv1d(6, out_channels=12,kernel_size=3,
+        layer_temp.append(nn.Conv1d(8, out_channels=12,kernel_size=3,
+                                        stride=1, padding=1))
+        layer_temp.append(nn.Conv1d(12, out_channels=12,kernel_size=3,
                                         stride=1, padding=1))
         layer_temp.append(nn.ReLU())
         layer_temp.append(nn.MaxPool1d(kernel_size=2, stride=2, padding=0))
 
         layer_temp.append(nn.Conv1d(12, out_channels=12,kernel_size=3,
                                         stride=1, padding=1))
+        layer_temp.append(nn.Conv1d(12, out_channels=12,kernel_size=3,
+                                        stride=1, padding=1))
         layer_temp.append(nn.ReLU())
         layer_temp.append(nn.MaxPool1d(kernel_size=2, stride=2, padding=0))
 
         layer_temp.append(nn.Flatten())
-        layer_temp.append(nn.Linear(in_features=384,out_features=16))
+        layer_temp.append(nn.Linear(in_features=96,out_features=16))
 
         self.encoder = torch.nn.Sequential(*layer_temp)
 
@@ -169,16 +143,9 @@ class SiameseNetwork_LMU(nn.Module):
 
         self.sigmoid = nn.Sigmoid()
 
-        
-    # def init_weights(self, m):
-    #     if isinstance(m, nn.Linear):
-    #         torch.nn.init.xavier_uniform_(m.weight)
-    #         m.bias.data.fill_(0.01)
-
     def forward_once(self, x):
         output = self.encoder(x)
         output = output.view(output.size()[0], -1)
-
         return output
 
     def forward(self, input1, input2):
@@ -186,9 +153,6 @@ class SiameseNetwork_LMU(nn.Module):
         # get two images' features
         output1 = self.forward_once(input1)
         output2 = self.forward_once(input2)
-
-        # concatenate both images' features
-        # output = torch.cat((output1, output2), 1)
         output=torch.norm((output1-output2), p=1, dim=1).unsqueeze(-1)
 
         # pass the concatenation to the linear layers
--- train_relation.py
+++ train_relation.py
@@ -13,161 +13,11 @@ from pathlib import Path
 
 from tqdm import tqdm
 import numpy as np
-from model_code.relation_model import transform_fully,SiameseNetwork
+from model_code.relation_model import *
 from data_mod.dataset import ECGDataset_pair, ECGDataset_few_shot
-# from dataset_alldata import ECGDataset_IE
 from utils import *
 from sklearn.metrics import confusion_matrix
 from torchsummary import summary
-
-
-# def evaluate_on_feature(dataloader,feature_net, net, args, criterion, device,prototype):
-#     print('Validating...')
-#     with open(args.multi_label_binarizer, 'rb') as tokenizer:
-#         mlb=pickle.load(tokenizer)
-#     feature_net.eval()
-#     net.eval()
-#     running_loss = 0
-#     output_list, labels_list = [], []
-#     for _, (data,labels) in enumerate(tqdm(dataloader)):
-#         data,labels= data.float().to(device), labels.float().to(device)
-#         prototype_embed=[]
-#         for i in prototype.keys():
-#             prototype_temp=feature_net(prototype[i])
-#             prototype_embed.append(torch.mean(prototype_temp,dim=0).unsqueeze(0))
-#         prototype_embed=torch.cat(prototype_embed,dim=0)
-#         batch_embed=feature_net(data)
-#         prototype_embed_ext=prototype_embed.unsqueeze(0).repeat(batch_embed.shape[0],1,1,1)
-#         batch_embed_ext=batch_embed.unsqueeze(0).repeat(5,1,1,1)
-#         batch_embed_ext = torch.transpose(batch_embed_ext,0,1)
-#         relation_pairs = torch.cat((prototype_embed_ext,batch_embed_ext),2).view(-1,128*2,79)
-#         output=net(relation_pairs).view(-1,5)
-#         loss = criterion(output, labels)
-#         running_loss += loss.item()
-#         output_list.append(output.data.cpu().numpy())
-#         labels_list.append(labels.data.cpu().numpy())
-
-
-
-    # for _, (data,labels) in enumerate(tqdm(dataloader)):
-    #     data,labels= data.float().to(device), labels.float().to(device)
-    #     feature=feature_net(data)
-    #     output_f=torch.tensor([]).to(device)
-    #     for key in prototype.keys():
-    #         output= net(feature)
-    #         embed=torch.abs(feature-prototype[key])
-    #         output=net(embed)
-    #         output = torch.sigmoid(output)
-    #         output_f=torch.cat((output_f,output),dim=1)
-    #     loss = criterion(output_f, labels)
-    #     running_loss += loss.item()
-    #     output_list.append(output_f.data.cpu().numpy())
-    #     labels_list.append(labels.data.cpu().numpy())
-
-
-#     loss_total=running_loss/len(dataloader)
-#     print('Loss: %.4f' % loss_total)
-#     y_trues = np.vstack(labels_list)
-#     y_scores = np.vstack(output_list)
-#     auc=cal_auc(y_trues,y_scores)
-#     if (args.phase=="train" and auc>args.best_metric):
-#         args.best_metrics=auc
-#         torch.save(net.state_dict(), args.model_path)
-#         torch.save(feature_net.state_dict(), args.feature_model_path)
-#         print("Saved")
-    
-#     print("Macro Precision: %.3f, Macro Recall: %.3f, Macro F1 score: %.3f, Macro AUC: %.3f, with threshold: %.3f" % cal_scores(y_trues,y_scores))
-  
-#     return y_trues,y_scores,loss_total
-
-# def train_on_feature(dataloader, feature_net,net, args, criterion, epoch, scheduler, optimizer, device,prototype):
-#     with open(args.multi_label_binarizer, 'rb') as tokenizer:
-#         mlb=pickle.load(tokenizer)
-#     print('Training epoch %d:' % epoch)
-#     running_loss = 0
-#     output_list, labels_list = [], []
-#     for _, (data,labels) in enumerate(tqdm(dataloader)):
-#         data,labels= data.float().to(device), labels.float().to(device)
-#         prototype_embed=[]
-#         for i in prototype.keys():
-#             prototype_temp=feature_net(prototype[i])
-#             prototype_embed.append(torch.mean(prototype_temp,dim=0).unsqueeze(0))
-#         prototype_embed=torch.cat(prototype_embed,dim=0)
-#         batch_embed=feature_net(data)
-#         prototype_embed_ext=prototype_embed.unsqueeze(0).repeat(batch_embed.shape[0],1,1,1)
-#         batch_embed_ext=batch_embed.unsqueeze(0).repeat(5,1,1,1)
-#         batch_embed_ext = torch.transpose(batch_embed_ext,0,1)
-#         relation_pairs = torch.cat((prototype_embed_ext,batch_embed_ext),2).view(-1,128*2,79)
-#         output=net(relation_pairs).view(-1,5)
-#         loss = criterion(output, labels)
-#         optimizer.zero_grad()
-#         loss.backward()
-#         optimizer.step()
-#         running_loss += loss.item()
-
-    # for _, (data,labels) in enumerate(tqdm(dataloader)):
-    #     data,labels= data.float().to(device), labels.float().to(device)
-    #     labels=torch.argmax(labels,axis=1)
-    #     feature = feature_net(data)
-    #     for key in prototype.keys():
-    #         net.train()
-    #         embed=torch.abs(feature-prototype[key])
-    #         output=net(embed)
-    #         output = torch.sigmoid(output)
-    #         labels_single=(labels==key).float().unsqueeze(1)
-    #         loss = criterion(output, labels_single)
-    #         optimizer.zero_grad()
-    #         loss.backward(retain_graph=True)
-    #         optimizer.step()
-    #         running_loss += loss.item()
-
-
-    # print(optimizer.param_groups[0]['lr'])
-    # scheduler.step()
-    # loss_total=running_loss/(len(dataloader)*len(prototype.keys()))
-    # print('Loss: %.4f' % loss_total)
-    # return loss_total
-
-
-
-# def train_on_feature_10fold(args,name,feature_model,shot):
-    #         print("Evaluate")
-    #         feature_net.load_state_dict(torch.load(args.feature_model_path,map_location=device))
-    #         net.load_state_dict(torch.load(args.model_path, map_location=device))
-    #         prototype={0:[],1:[],2:[],3:[],4:[]}
-    #         for data, label in train_loader:
-    #             data= data.float().to(device)
-    #             # out=feature_net(data)
-    #             for ind,labelty in enumerate(label):
-    #                 prototype[np.where(labelty)[0][0]].append(data[ind].unsqueeze(0))
-    #         for key in prototype.keys():
-    #             prototype[key]=torch.cat(prototype[key], dim=0)
-    #         y_true,y_score,loss_test=evaluate_on_feature(test_loader,feature_net, net, args, criterion, device,prototype)
-    #         Final_output.append(y_score)
-    #         Final_true.append(y_true)
-
-    # if args.phase != 'train':
-    #     Final_output=np.vstack(Final_output)
-    #     Final_true=np.vstack(Final_true)
-    #     print(Final_output.shape)
-    #     print(Final_true.shape)
-    #     path=(result_path+args.model_name+"_Final")
-    #     save_result(Final_true,Final_output,path)
-    
-# def train_shot_on_feature(args,dataset,feature_model):
-#     shots=[5,10,20,50]
-#     for shot in shots:
-#         train_on_feature_10fold(args,dataset,feature_model,shot)
-
-# def folder_generate(name):
-#     path=os.path.join("./models",name)
-#     if not os.path.exists(path):
-#         os.makedirs(path)
-
-#     path=os.path.join("./result",name)
-#     if not os.path.exists(path):
-#         os.makedirs(path)
-
 
 def train(dataloader, net, arg, criterion, epoch, optimizer, device):
     print('Training epoch %d:' % epoch)
@@ -187,13 +37,8 @@ def train(dataloader, net, arg, criterion, epoch, optimizer, device):
         output_list.append(output.data.cpu().numpy())
         labels_list.append(labels.data.cpu().numpy())
     print(optimizer.param_groups[0]['lr'])
-    # scheduler.step()
     loss_total=running_loss/(len(dataloader))
     print('Loss: %.4f' % loss_total)
-    # y_trues = np.vstack(labels_list)
-    # y_scores = np.vstack(output_list)
-    # torch.save(encoder_net.state_dict(), arg.encoder_model_path)   
-    # print("Macro Precision: %.3f, Macro Recall: %.3f, Macro F1 score: %.3f, Macro AUC: %.3f, with threshold: %.3f" % cal_scores(y_trues,y_scores))
     if loss_total<arg.best_metric:
         arg.best_metric=loss_total
         arg.patience = 100
@@ -241,6 +86,30 @@ def evaluation(dataloader,net,arg,criterion,shot,device):
 
 
 def train_relation(arg,name):
+
+    arg.model_path=Path("./models",name,arg.model_name+".pth")
+    arg.result_path=Path("./result",name)
+    seed = arg.seed
+
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+
+    if arg.use_gpu and torch.cuda.is_available():
+        device = torch.device('cuda:0')
+    else:
+        device = 'cpu'
+
+    print(device) 
+    print("Output on:",arg.model_name)
+    if arg.model_name=="Siamese_Sembed":
+        net=Siamese_Sembed().to(device)
+    elif arg.model_name=="Siamese_LMU":
+        net=Siamese_LMU().to(device)
+    summary(net,[(1,259),(1,259)])
+
+
     if arg.phase=="Train":
         data_path_1=Path("./data",name,"train","data",name+"1.npy")
         data_path_2=Path("./data",name,"train","data",name+"2.npy")
@@ -248,41 +117,15 @@ def train_relation(arg,name):
         support_path=Path("./data",name,"val","data",name+"_support_1_shot.npy")
         query_path=Path("./data",name,"val","data",name+"_query_1_shot.npy")
         label_val_path=Path("./data",name,"val","label",name+"_1_shot.npy")
-        # arg.transform_model_path=Path("./models",name,arg.transform_model_name+".pth")
-        # arg.encoder_model_path=Path("./models",name,arg.encoder_model_name+".pth")
-        arg.encoder_model_path=Path("./models",name,"SiameseNetwork"+".pth")
-        arg.result_path=Path("./result",name)
         train_dataset=ECGDataset_pair(data_path_1,data_path_2,label_path)
         train_loader = DataLoader(train_dataset, batch_size=arg.batch_size, shuffle=True, num_workers=arg.num_workers, pin_memory=True)
         val_dataset=ECGDataset_few_shot(support_path,query_path,label_val_path)
         val_loader = DataLoader(val_dataset, batch_size=arg.batch_size, shuffle=False, num_workers=arg.num_workers, pin_memory=True)
-        seed = arg.seed
-        torch.manual_seed(seed)
-        if torch.cuda.is_available():
-            torch.cuda.manual_seed_all(seed)
-        np.random.seed(seed)
-        if arg.use_gpu and torch.cuda.is_available():
-            device = torch.device('cuda:0')
-        else:
-            device = 'cpu'
-        print(device)
-        # if arg.transform_model_name=="fully-connected":
-        #     print("Train on:",arg.transform_model_name)
-        #     net =transform_fully().to(device)
-        # if arg.encoder_model_name=="Sembed":
-        #     print("Output on:",arg.encoder_model_name)
-        #     encoder_net =Sembedencoder().to(device)  
-        print("Output on:","SiameseNetwork")
-        net =SiameseNetwork().to(device)  
-        summary(net,[(1,259),(1,259)])
         optimizer = torch.optim.Adam(net.parameters(), lr=arg.lr)
-        # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 10, gamma=0.1)
         criterion = nn.BCELoss()
         print("train")
         if arg.resume:
-            # net.load_state_dict(torch.load(arg.transform_model_path,map_location=device))
-            # encoder_net.load_state_dict(torch.load(arg.encoder_model_path, map_location=device))
-            net.load_state_dict(torch.load(arg.encoder_model_path,map_location=device))
+            net.load_state_dict(torch.load(arg.model_path,map_location=device))
         train_loss=[]
         val_loss=[]
         arg.best_metric=100.0
@@ -295,38 +138,19 @@ def train_relation(arg,name):
             val_loss.append(val_loss_temp)
             if flag:
                 break
-        np.save(str(arg.result_path)+"/SiameseNetwork_train_loss.npy",train_loss)
-        np.save(str(arg.result_path)+"/SiameseNetwork_val_loss.npy",val_loss)
+        np.save(str(arg.result_path)+"/"+arg.model_name+"_train_loss.npy",train_loss)
+        np.save(str(arg.result_path)+"/"+arg.model_name+"_val_loss.npy",val_loss)
     else:
         shots=[1,5]
         for shot in shots:
             support_path=Path("./data",name,"test","data",name+"_support_"+str(shot)+"_shot.npy")
             query_path=Path("./data",name,"test","data",name+"_query_"+str(shot)+"_shot.npy")
             label_path=Path("./data",name,"test","label",name+"_"+str(shot)+"_shot.npy")
-            # arg.transform_model_path=Path("./models",name,arg.transform_model_name+".pth")
-            # arg.encoder_model_path=Path("./models",name,arg.encoder_model_name+".pth")
-            arg.encoder_model_path=Path("./models",name,"SiameseNetwork"+".pth")
-            arg.result_path=Path("./result",name)
             test_dataset=ECGDataset_few_shot(support_path,query_path,label_path)
             test_loader = DataLoader(test_dataset, batch_size=arg.batch_size, shuffle=False, num_workers=arg.num_workers, pin_memory=True)
-            if arg.use_gpu and torch.cuda.is_available():
-                device = torch.device('cuda:0')
-            else:
-                device = 'cpu'
-            print(device)
 
-            # if arg.transform_model_name=="fully-connected":
-            #     print("Train on:",arg.transform_model_name)
-            #     net =transform_fully().to(device)
-            # if arg.encoder_model_name=="Sembed":
-            #     print("Output on:",arg.encoder_model_name)
-            #     encoder_net =Sembedencoder().to(device)  
-            print("Output on:","SiameseNetwork")
-            net =SiameseNetwork().to(device)  
-            summary(net,[(1,259),(1,259)])
             criterion=nn.BCELoss()
-            # net.load_state_dict(torch.load(arg.transform_model_path,map_location=device))
-            net.load_state_dict(torch.load(arg.encoder_model_path, map_location=device))
+            net.load_state_dict(torch.load(arg.model_path, map_location=device))
             y_true,y_score,_=evaluation(test_loader,net,arg,criterion,shot,device)
             result_path=Path(arg.result_path,arg.encoder_model_name)
             save_result(y_true,y_score,result_path,shot)
@@ -344,8 +168,7 @@ if __name__=="__main__":
     database = os.path.basename(data_dir)
     dataset=[1,10]
     print(arg.data_dir)
-    print("Train on:",arg.encoder_model_name)
-    print("Train on:",arg.transform_model_name)
+    print("Train on:",arg.model_name)
     arg.phase="test"
     train_on_dataset(arg,dataset)
     
