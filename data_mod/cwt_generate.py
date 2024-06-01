4d5c9d7aa080f8fd931a4f9a426a47e7b75d4cca
mn12you <s1212mn@gmail.com>
2024-06-02 01:27:48 +0800

    modified:   data_mod/cwt_generate.py

--- data_mod/cwt_generate.py
+++ data_mod/cwt_generate.py
@@ -1,7 +1,18 @@
 from tfa_morlet56 import *
+import sys
+import os 
 from pathlib import Path
-import os
-
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+from args import parse_args
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import numpy as np
+import pickle as pkl
+from matplotlib import pyplot as plt
+from data_mod.dataset import ECGDataset_all
+from sklearn.model_selection import train_test_split
+import random
 
 def folder_generate(name):
     data_diff=["1","5","10","30","50","90","150"]
@@ -42,26 +53,15 @@ if __name__=="__main__":
     basepath="mitbih"
     random.seed(arg.seed)
     folder_generate(basepath)
-    # output_list=[]
-    # labels_list=[]
-    # for _, (data, labels) in enumerate(tqdm(train_loader)):
-    #     output_list.append(data)
-    #     labels_list.append(labels)
-    # y_data = np.vstack(output_list)
-    # y_label = np.vstack(labels_list)
-    # y_label=np.squeeze(y_label,axis=None)
-    # print(y_label.shape)
-    # print(y_data.shape)
     data_diff=["1","5","10","30","50","90","150"]
     for diff in data_diff:
         base_path=path=Path("./data",basepath+"_"+diff)
+        base_path_save=path=Path("./data",basepath+"_"+diff)
         train_data_path=Path(base_path,"train","data",basepath+"_"+diff+".npy")
         train_label_path=Path(base_path,"train","label",basepath+"_"+diff+".npy")
         base_path=path=Path("./data",basepath+"_"+diff+"_"+"pair")
         train_data_path_save_1=Path(base_path,"train","data",basepath+"_"+diff+"_"+"pair1"+".npy")
-        train_data_path_save_2=Path(base_path,"train","data",basepath+"_"+diff+"_"+"pair2"+".npy")
-        train_label_path_save=Path(base_path,"train","label",basepath+"_"+diff+"_"+"pair"+".npy")
-        train_dataset=ECGDataset_all(train_data_path,train_label_path)
+
         train_loader = DataLoader(train_dataset, batch_size=64, shuffle=False, num_workers=8)
 
         output_list=[]
